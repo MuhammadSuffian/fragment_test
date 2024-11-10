cafb125549569d4561b59c5c6adf0e92c572303b
MuhammadSuffian <muhammad.suffian.5959@gmail.com>
2024-11-10 18:11:07 +0500

    v1

--- app/src/main/java/com/example/fragment_test/MainActivity.java
+++ app/src/main/java/com/example/fragment_test/MainActivity.java
@@ -14,26 +14,31 @@ import androidx.fragment.app.FragmentTransaction;
 public class MainActivity extends AppCompatActivity {
     Button btn1,btn2;
     Fragment frag;
+    FragmentTransaction ft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         btn1=findViewById(R.id.btn1);
         btn2=findViewById(R.id.btn2);
+        frag=new frag1();
+        ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment,frag);
+        ft.addToBackStack(null);
+        ft.commit();
 
         btn1.setOnClickListener(v->{
+            ft=getSupportFragmentManager().beginTransaction();
             frag=new frag1();
-            FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.fragment,frag);
-            ft.addToBackStack(null);
+//            ft.addToBackStack(null);
             ft.commit();
         });
         btn2.setOnClickListener(v->{
+            ft=getSupportFragmentManager().beginTransaction();
             frag=new frag2();
-            FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.fragment,frag);
-            ft.addToBackStack(null);
+//            ft.addToBackStack(null);
             ft.commit();
         });
 
